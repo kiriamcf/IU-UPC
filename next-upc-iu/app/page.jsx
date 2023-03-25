c94ffc9d531c017baa25b7d6f27059cf43450899
Kiriam C. Forés <kiriamcf@gmail.com>
2023-03-25 19:13:31 +0100

    fix keys loop

--- next-upc-iu/app/page.jsx
+++ next-upc-iu/app/page.jsx
@@ -5,12 +5,12 @@ import Slider from './components/slider'
 
 export default function Home() {
   const landing_images = [
-    {url: "/landing_1.jpg", alt: "Arròs i gambes"},
-    {url: "/landing_2.jpg", alt: "Carn i verdures"},
-    {url: "/landing_3.jpg", alt: "Carn estil japonès"},
-    {url: "/landing_4.jpg", alt: "Sopa de verdures"},
-    {url: "/landing_5.jpg", alt: "Arròs amb carn"},
-    {url: "/landing_6.jpg", alt: "Carn amb verdures"},
+    {id: 1, url: "/landing_1.jpg", alt: "Arròs i gambes"},
+    {id: 2, url: "/landing_2.jpg", alt: "Carn i verdures"},
+    {id: 3, url: "/landing_3.jpg", alt: "Carn estil japonès"},
+    {id: 4, url: "/landing_4.jpg", alt: "Sopa de verdures"},
+    {id: 5, url: "/landing_5.jpg", alt: "Arròs amb carn"},
+    {id: 6, url: "/landing_6.jpg", alt: "Carn amb verdures"},
   ];
 
   return (
@@ -27,12 +27,12 @@ export default function Home() {
           <div className="flex items-center justify-center">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7">
               {landing_images.map(imatge => (
-                <Image src={imatge.url} alt={imatge.alt} width={300} height={300} className="h-[250px] object-cover rounded" />
+                <Image key={imatge.id} src={imatge.url} alt={imatge.alt} width={300} height={300} className="h-[250px] object-cover rounded" />
               ))}
             </div>
           </div>
         </div>
-        <iframe className="w-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1488.6222323815655!2d1.827872609130784!3d41.7368196264712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a45806394f06e3%3A0xd61cd53aaa1aaf0d!2sRestaurante%20UPC%2C%20Manresa!5e0!3m2!1ses!2ses!4v1678902887030!5m2!1ses!2ses" height="500" style={{ border:0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe className="w-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1488.6222323815655!2d1.827872609130784!3d41.7368196264712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a45806394f06e3%3A0xd61cd53aaa1aaf0d!2sRestaurante%20UPC%2C%20Manresa!5e0!3m2!1ses!2ses!4v1678902887030!5m2!1ses!2ses" height="500" style={{ border:0 }} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </main>
 
       <Footer />
